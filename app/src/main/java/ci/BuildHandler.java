7dd1a284ce1d35106b95c224f713ccf86c1e28fd
LMC117 <2295699210@qq.com>
2023-02-04 11:25:03 +0100

    feat: make an initial framework for issue #3

---
+++ app/src/main/java/ci/BuildHandler.java
@@ -0,0 +1,19 @@
+package ci;
+
+import org.eclipse.jetty.server.Request;
+
+import java.io.File;
+
+public class BuildHandler {
+
+    /**
+     * Building cloned repository.
+     */
+    public static void runBuild(Request baseRequest) {
+
+    }
+
+    private static String runCheck(File path){
+
+    }
+}
