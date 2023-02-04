24dcf54f9f32054ddd34358e5f81fe58078f9a74
Ali Shahwali <ali_zinq@hotmail.com>
2023-02-04 18:21:27 +0100

    feat: Resolved issue #15
    Server can now receive and process POST requests for starting CI pipeline.

--- app/src/main/java/server/ContinuousIntegrationServer.java
+++ app/src/main/java/server/ContinuousIntegrationServer.java
@@ -5,10 +5,16 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import ci.Repository;
 
 public class ContinuousIntegrationServer extends AbstractHandler {
     public void handle(String target,
@@ -16,18 +22,28 @@ public class ContinuousIntegrationServer extends AbstractHandler {
             HttpServletRequest request,
             HttpServletResponse response)
             throws IOException, ServletException {
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
-        baseRequest.setHandled(true);
+        try {
+            response.setContentType("text/html;charset=utf-8");
+            // response.setStatus(HttpServletResponse.SC_OK);
+            baseRequest.setHandled(true);
+            String method = request.getMethod();
 
-        System.out.println(target);
+            switch (method.toUpperCase()) {
+                case "GET":
+                    // Currently not implemented
+                    break;
+                case "POST":
+                    handleCIRequest(request, response);
+                    break;
+                default:
+                    break;
+            }
+        } catch (IOException e) {
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                    "An exception has occured on the server causing an error.");
 
-        // here you do all the continuous integration tasks
-        // for example
-        // 1st clone your repository
-        // 2nd compile the code
+        }
 
-        response.getWriter().println("CI job done");
     }
 
     // used to start the CI server in command line
@@ -36,5 +52,34 @@ public class ContinuousIntegrationServer extends AbstractHandler {
         server.setHandler(new ContinuousIntegrationServer());
         server.start();
         server.join();
+    }
+
+    /**
+     * Handles POST requests for CI.
+     * 
+     * @param request  JSON object
+     * @param response
+     * @throws IOException
+     */
+    private void handleCIRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String payload = request.getReader().lines().collect(Collectors.joining());
+        try {
+            JSONObject json = new JSONObject(payload);
+            String repoName = json.get("name").toString();
+
+            Repository repo = new Repository(json.get("id").toString(), repoName,
+                    json.get("url").toString(), json.get("branch").toString(), json.get("user").toString());
+            repo.buildRepository();
+            /**
+             * Add test when https://github.com/DD2480-Group-5/Assignment-2/issues/14 is
+             * resolved
+             */
+
+            response.setStatus(HttpServletResponse.SC_OK);
+
+        } catch (JSONException e) {
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                    "Something went wrong while processing the POST request.");
+        }
     }
 }
