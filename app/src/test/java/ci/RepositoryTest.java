63d8a9ac776206dd5454f7aca7c5913aff1acf36
BillXu0424 <1065602877@qq.com>
2023-02-08 15:45:55 +0100

    test. issue #64 resolved: add unit tests for compile error and test error.

--- app/src/test/java/ci/RepositoryTest.java
+++ app/src/test/java/ci/RepositoryTest.java
@@ -3,6 +3,7 @@ package ci;
 import org.junit.jupiter.api.Test;
 
 import ci.GitHubAPIHandler.STATE;
+import ci.Repository.BUILD_STATE;
 
 import java.io.File;
 
@@ -36,9 +37,9 @@ class RepositoryTest {
 
     @Test
     /**
-     * This test is used to test the build result. 
+     * This test is used to test the build result for success. 
      */
-    public void buildRepoTest() {
+    public void buildRepoSuccessTest() {
         String id = "31113e4dc5da9b2e01c9c4ee85d2c5c4e9aa96b3";
         String name = "Assignment-1";
         String url = "git@github.com:DD2480-Group-5/Assignment-1.git";
@@ -50,6 +51,48 @@ class RepositoryTest {
 
         System.out.println(status);
         assertSame(STATE.SUCCESS, status);
+
+        repo.deleteRepository();
+    }
+
+    @Test
+    /**
+     * This test is used to test the build result for compile error.
+     */
+    public void buildRepoCompileErrorTest() {
+        String id = "c5d4507af53232f3965d5cb7614bfb263abe7e44";
+        String name = "Assignment-1";
+        String url = "git@github.com:DD2480-Group-5/Assignment-1.git";
+        String branch = "ci_test2";
+        String user = "DD2480-Group-5";
+        
+        Repository repo = new Repository(id, name, url, branch, user);
+        repo.buildRepository();
+        BUILD_STATE buildState = repo.getBuildState();
+
+        System.out.println(buildState);
+        assertSame(BUILD_STATE.COMPILE_ERROR, buildState);
+
+        repo.deleteRepository();
+    }
+
+    @Test
+    /**
+     * This test is used to test the build result for test error.
+     */
+    public void buildRepoTestErrorTest() {
+        String id = "1ec066f4a168c9d93f6ef01ecddffb3cce61cf0a";
+        String name = "Assignment-2";
+        String url = "git@github.com:DD2480-Group-5/Assignment-2.git";
+        String branch = "assessment";
+        String user = "DD2480-Group-5";
+
+        Repository repo = new Repository(id, name, url, branch, user);
+        repo.buildRepository();
+        BUILD_STATE buildState = repo.getBuildState();
+
+        System.out.println(buildState);
+        assertSame(BUILD_STATE.TEST_ERROR, buildState);
 
         repo.deleteRepository();
     }
